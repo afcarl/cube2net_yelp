aa8e5cec3ff127832b114737bc56ed3165749494
Carl Yang <carlyang@wirelessprv-10-193-82-214.near.illinois.edu>
2018-02-05 19:44:37 -0600

    revise cube

--- yelp_cube/cube_construction.py
+++ yelp_cube/cube_construction.py
@@ -12,8 +12,8 @@ class YelpCube(object):
 	def __init__(self, params):
 		self.params = params
 
-		self.business = []
-		self.user = []
+		self.business_id = {}
+		self.user_id = {}
 		self.business_user = []
 		self.user_business = []
 		self.review_business = []
@@ -27,10 +27,8 @@ class YelpCube(object):
 
 	def step1(self):
 		#input businesses
-		business_all = set()
-		business_ca_takeout = set()
-		business_il_goodforkids = set()
 		num_business = 0
+		valid_business = 0
 		with open(self.params['yelp_business'], 'r') as f:
 			for line in f:
 				p = json.loads(line)
@@ -44,30 +42,25 @@ class YelpCube(object):
 				or ('categories' not in p):
 					continue
 
-				business_all.add(p['business_id'])
-				if p['state'].lower() == 'ca' and 'RestaurantsTakeOut' in p['attributes'] and p['attributes']['RestaurantsTakeOut']:
-					business_ca_takeout.add(p['business_id'])
-				if p['state'].lower() == 'il' and 'GoodForKids' in p['attributes'] and p['attributes']['GoodForKids']:
-					business_il_goodforkids.add(p['business_id'])
+				self.business_id[p['business_id']] = valid_business
+				valid_business += 1
 
 				for cat in map(lambda x: x.lower(), p['categories']):
 					if cat not in self.category_name:
 						self.category_name.append(cat)
 						self.category_business.append(set())
-					self.category_business[self.category_name.index(cat)].add(p['business_id'])
+					self.category_business[self.category_name.index(cat)].add(valid_business-1)
 				if p['city'] not in self.city_name:
 					self.city_name.append(p['city'])
 					self.city_business.append(set())
-				self.city_business[self.city_name.index(p['city'])].add(p['business_id'])
+				self.city_business[self.city_name.index(p['city'])].add(valid_business-1)
 
-		self.business = list(business_all)
-		self.business_user = [set() for i in range(len(self.business))]
-		del business_all
-		print('finised input businesses: %d/%d' % (len(self.business), num_business) )
+		self.business_user = [set() for i in range(len(self.business_id))]
+		print('finised input businesses: %d/%d' % (valid_business, num_business) )
 
 		#input users
-		user_all = set()
 		num_user = 0
+		valid_user = 0
 		with open(self.params['yelp_user'], 'r') as f:
 			for line in f:
 				p = json.loads(line)
@@ -77,13 +70,11 @@ class YelpCube(object):
 				or (int(p['review_count']) < 10):
 					continue
 
-				user_all.add(p['user_id'])
+				self.user_id[p['user_id']] = valid_user
+				valid_user += 1
 
-		self.user = list(user_all)
-		self.user_business = [set() for i in range(len(self.user))]
-		del user_all
-		print('finised input users: %d/%d' % (len(self.user), num_user) )
-
+		self.user_business = [set() for i in range(len(self.user_id))]
+		print('finised input users: %d/%d' % (valid_user, num_user) )
 
 		#input reviews
 		num_review = 0
@@ -91,8 +82,6 @@ class YelpCube(object):
 			for line in f:
 				p = json.loads(line)
 				num_review += 1
-				if num_review % 1000 == 0:
-					print('%d/%d reviews processed' % (len(self.review_business), num_review))
 				if ('business_id' not in p) \
 				or ('user_id' not in p) \
 				or ('text' not in p) \
@@ -100,13 +89,13 @@ class YelpCube(object):
 					continue
 			
 				try:
-					bid = self.business.index(p['business_id'])
-					uid = self.user.index(p['user_id'])
-				except ValueError:
+					bid = self.business_id[p['business_id']]
+					uid = self.user_id[p['user_id']]
+				except KeyError:
 					continue
 
 				cf.write((p['text'].replace('\n', '')+'\n').encode('utf-8'))
-				self.review_business.append(p['business_id'])
+				self.review_business.append(bid)
 				self.business_user[bid].add(uid)
 				self.user_business[uid].add(bid)
 
@@ -145,43 +134,46 @@ class YelpCube(object):
 		if not os.path.exists('models/basenet.pkl'):
 			print('generating basenet.')
 			basenet = {}
-			basenet['set0_net0'] = set()
-			basenet['set0_net1'] = set()
-			basenet['set1_net0'] = set()
-			basenet['set1_net1'] = set()
-			basenet['set0_test'] = set()
-			basenet['set1_test'] = set()
+			basenet['set0_business'] = set()
+			basenet['set0_link'] = set()
+			basenet['set1_business'] = set()
+			basenet['set1_link'] = set()
 
-			for b in map(lambda x: self.business.index(x), self.business_ca_takeout):
+			business_ca_takeout = set()
+			business_il_goodforkids = set()
+
+			with open(self.params['yelp_business'], 'r') as f:
+				for line in f:
+					p = json.loads(line)
+					if ('business_id' not in p) \
+					or (p['business_id'] not in self.business_id):
+						continue
+
+				if p['state'].lower() == 'ca' and 'RestaurantsTakeOut' in p['attributes'] and p['attributes']['RestaurantsTakeOut']:
+					business_ca_takeout.add(valid_business-1)
+				if p['state'].lower() == 'il' and 'GoodForKids' in p['attributes'] and p['attributes']['GoodForKids']:
+					business_il_goodforkids.add(valid_business-1)
+
+
+			print('got %d ca_takeout business, %d il_goodforkids business' % (len(business_ca_takeout), len(business_il_goodforkids)))
+
+			for b in self.business_ca_takeout:
 				if random.random() < 0.5:
-					basenet['set0_net0'].add(b)
+					basenet['set0_business'].add(b)
 					for u in self.business_user[b]:
 						if random.random() < 0.2:
-							basenet['set0_test'].add((b, u))
+							basenet['set0_link'].add((b, u))
 
-			starttime = time.time()
-			for b in basenet['set0_net0']:
-				for u in self.business_user[b]:
-					for b1 in self.user_business[u]:
-						basenet['set0_net1'].add(b1)
+			print('generated basenet 0 with %d business and %d held links.' %(len(basenet['set0_business']), len(basenet['set0_link'])))
 
-			print('generated basenet 0 with net0/net1/test as %d/%d/%d' %(len(basenet['set0_net0']), len(basenet['set0_net1']), len(basenet['set0_test'])))
-			print('time spent %f s' % (time.time()-starttime))
-
-			for b in map(lambda x: self.business.index(x), self.business_il_goodforkids):
+			for b in self.business_il_goodforkids:
 				if random.random() < 0.5:
-					basenet['set1_net0'].add(b)
+					basenet['set1_business'].add(b)
 					for u in self.business_user[b]:
 						if random.random() < 0.2:
-							basenet['set1_test'].add((b, u))
+							basenet['set1_link'].add((b, u))
 
-			starttime = time.time()
-			for b in basenet['set1_net0']:
-				for u in self.business_user[b]:
-					for b1 in self.user_business[u]:
-						basenet['set1_net1'].add(b1)
-			print('generated basenet 1 with net0/net1/test as %d/%d/%d' %(len(basenet['set1_net0']), len(basenet['set1_net1']), len(basenet['set1_test'])))
-			print('time spent %f s' % (time.time()-starttime))
+			print('generated basenet 1 with %d business and %d held links.' %(len(basenet['set1_business']), len(basenet['set1_link'])))
 			
 			with open('models/basenet.pkl', 'wb') as f:
 				pickle.dump(basenet, f)
